7606641072d55eaee08f9a4c10e278be9e1bdd5b
luochenLing <864684766@qq.com>
2020-03-18 22:58:23 +0800

    add project

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ module.exports = {
     // 将 entry 指向应用程序的 server / client 文件
     entry: `./src/entry-${target}.js`,
     // 对 bundle renderer 提供 source map 支持
-
+    devtool: 'source-map',
     // 这允许 webpack 以 Node 适用方式处理动态导入(dynamic import)，
     // 并且还会在编译 Vue 组件时告知 `vue-loader` 输送面向服务器代码(server-oriented code)。
     target: TARGET_NODE ? "node" : "web",
